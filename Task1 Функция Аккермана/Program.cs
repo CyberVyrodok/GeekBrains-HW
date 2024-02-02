f1c26ee02323a6e1d873c7a57167c14ad73f37dc
Selectel Git User <git-user@selectel.ru>
2024-02-02 22:05:11 +0300

    GeekBrains HW done

---
+++ Task1 Функция Аккермана/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+Console.WriteLine("Введите 1-ое неотрицательное число: ");
+string input1 = Console.ReadLine();
+Console.WriteLine("Введите 2-ое неотрицательное число: ");
+string input2 = Console.ReadLine();
+BigInteger n = BigInteger.Parse(input1);
+BigInteger m = BigInteger.Parse(input2);
+Console.WriteLine($"Функция Аккермана А({n}, {m}) = {Ack(n, m)}");
+
+static BigInteger Ack(BigInteger num1, BigInteger num2)
+{
+    if (num1 == 0)
+    {
+        return BigInteger.Add(num2,1);
+    }
+    else if (num2 == 0)
+    {
+        return Ack(BigInteger.Subtract(num1,1), 1);
+    }
+    else
+    {
+        return Ack(BigInteger.Subtract(num1,1), Ack(num1, BigInteger.Subtract(num2,1)));
+    }
+}
---
+++ Task2 Обратный массив/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Сколько чисел будет в массиве?");
+int size = Convert.ToInt32(Console.ReadLine());
+Random rnd = new Random();
+int[] array = new int[size];
+
+Console.Write("Сгенерированный массив: ");
+PrintGenerationArray(array, rnd, 0);
+
+Console.WriteLine();
+Console.Write("Обратный массив: ");
+PrintReverseArray(array, size - 1);
+
+void PrintReverseArray(int[] numbers, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write($"{numbers[index]} ");
+        PrintReverseArray(array, index - 1);
+    }
+}
+
+void PrintGenerationArray(int[] numbs, Random rnd, int currentIndex)
+{
+    if (currentIndex < numbs.Length)
+    {
+        numbs[currentIndex] = rnd.Next(1, 1001);
+        Console.Write($"{numbs[currentIndex]} ");
+        PrintGenerationArray(numbs, rnd, currentIndex + 1);
+    }
+}
